2d4c7e2b430b579741924e13f5283a41aea2f869
Adnaan Zubair Shah <136037246+adnaanzshah@users.noreply.github.com>
2024-09-19 12:33:26 +0530

    solved

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,33 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int nums[]={},target;
-        
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+    public:
+    vector<int> twoSum(vector<int> &nums,int target){
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
     }
 };
+int main(){
+
+    cout << endl;
+    Solution s;
+    // vector<int> a={2,4,5,6};
+    // int t=6;
+    vector<int> v=s.twoSum(a,t);
+    for (int i : v) {
+        cout << i << " ";
+    }
+    return 0;
+}
